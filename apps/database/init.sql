5427a043d3f8b9ad12e4748faa4abe8bae76bcc7
Bernardo <balvesdeoliveira3@gmail.com>
2025-02-20 23:55:08 -0300

    corriging possible future problems

--- apps/database/init.sql
+++ apps/database/init.sql
@@ -13,7 +13,7 @@ LANGUAGE plpgsql;
 CREATE TABLE IF NOT EXISTS agendamentos (
     id SERIAL PRIMARY KEY,
     cliente VARCHAR(255) NOT NULL,
-    data DATE NOT NULL,
+    datas DATE NOT NULL,
     hora TIME NOT NULL,
     criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
